e13a89e2fa2b3fd29d434ae8f465cd270cd7cfcf
Emeric <emeric.janowski@it-ce.fr>
2021-01-25 13:53:41 +0100

    partie 2 d

--- app/src/main/java/com/example/isen_2021/HomeActivity.kt
+++ app/src/main/java/com/example/isen_2021/HomeActivity.kt
@@ -2,10 +2,20 @@ package com.example.isen_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.example.isen_2021.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.starter.setOnClickListener {
+            Toast.makeText(this, getString(R.string.toast_content), Toast.LENGTH_LONG).show()
+        }
     }
 }
