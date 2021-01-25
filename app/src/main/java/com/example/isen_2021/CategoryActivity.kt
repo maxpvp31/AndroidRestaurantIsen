63f4c8ccd55a9448699b06b794c1101fcb1dcf04
Emeric <emeric.janowski@it-ce.fr>
2021-01-25 16:15:07 +0100

    category title

--- app/src/main/java/com/example/isen_2021/CategoryActivity.kt
+++ app/src/main/java/com/example/isen_2021/CategoryActivity.kt
@@ -3,17 +3,34 @@ package com.example.isen_2021
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.isen_2021.databinding.ActivityCategoryBinding
 
 enum class ItemType {
     STARTER, MAIN, DESSERT
 }
 
 class CategoryActivity : AppCompatActivity() {
+
+    private lateinit var bindind: ActivityCategoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category)
+        bindind = ActivityCategoryBinding.inflate(layoutInflater)
+        setContentView(bindind.root)
+
+        val selectedItem = intent.getSerializableExtra(HomeActivity.CATEGORY_NAME) as? ItemType
+        bindind.categoryTitle.text = getCategoryTitle(selectedItem)
 
         Log.d("lifecycle", "onCreate")
+    }
+
+    private fun getCategoryTitle(item: ItemType?): String {
+        return when(item) {
+            ItemType.STARTER -> getString(R.string.starter)
+            ItemType.MAIN -> getString(R.string.main)
+            ItemType.DESSERT -> getString(R.string.dessert)
+            else -> ""
+        }
     }
 
     override fun onResume() {
