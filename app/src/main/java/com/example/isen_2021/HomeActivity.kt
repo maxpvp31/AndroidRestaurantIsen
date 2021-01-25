ab22607f9572cc611189e8990477a5539baf900e
Emeric <emeric.janowski@it-ce.fr>
2021-01-25 10:03:55 +0100

    first commit

---
+++ app/src/main/java/com/example/isen_2021/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.isen_2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
